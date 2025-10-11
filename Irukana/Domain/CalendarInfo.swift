e456dc9eaa42c6249bca981f2b4817fc70f14778
TAKE00007 <xshun1590@icloud.com>
2025-10-11 19:33:45 +0900

    CalendarInfosエンティティを定義

---
+++ Irukana/Domain/CalendarInfo.swift
@@ -0,0 +1,14 @@
+//
+//  Calendar.swift
+//  Irukana
+//
+//  Created by 大竹駿 on 2025/10/11.
+//
+
+import Foundation
+
+struct CalendarInfo {
+    let id: UUID
+    let groupId: UUID
+    var name: String
+}
