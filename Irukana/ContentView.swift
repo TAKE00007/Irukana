6afe16a650c885b897178475b2f3d3a072d50410
TAKE00007 <xshun1590@icloud.com>
2025-10-11 17:08:35 +0900

    firstcommit

--- Irukana/ContentView.swift
+++ Irukana/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("これはご飯のいるいらないを共有するアプリです")
         }
         .padding()
     }
