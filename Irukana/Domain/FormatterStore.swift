449b8435796c9334f57f7ff9da95b21f22e77558
TAKE00007 <xshun1590@icloud.com>
2025-10-29 22:17:28 +0900

    Format系をまとめた

---
+++ Irukana/Domain/FormatterStore.swift
@@ -0,0 +1,27 @@
+//
+//  FormatterStore.swift
+//  Irukana
+//
+//  Created by 大竹駿 on 2025/10/29.
+//
+
+import Foundation
+
+public struct FormatterStore {
+    /// yyyy-MM-dd
+    static var yyyyMMddDashStyle: Date.ISO8601FormatStyle {
+        .iso8601.year().month().day().dateSeparator(.dash)
+    }
+    
+    static func startOfDay(_ date: Date) -> Date {
+        let cal = Calendar(identifier: .gregorian)
+        return cal.startOfDay(for: date)
+    }
+    
+    static func monthRange(for date: Date) -> (start: Date, end: Date) {
+        let cal = Calendar(identifier: .gregorian)
+        let start = cal.date(from: cal.dateComponents([.year, .month], from: date))!
+        let end = cal.date(byAdding: DateComponents(month: 1), to: start)!
+        return (start, end)
+    }
+}
