88dc1c5e1e9972a3c58189a95478d94c9c8d695b
TAKE00007 <xshun1590@icloud.com>
2025-10-30 21:06:37 +0900

    通知画面のUI層を作った

---
+++ Irukana/Presentation/Notification/NotificationAction.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationAction.swift
+//  Irukana
+//
+//  Created by 大竹駿 on 2025/10/30.
+//
+
+import Foundation
---
+++ Irukana/Presentation/Notification/NotificationReducer.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationReducer.swift
+//  Irukana
+//
+//  Created by 大竹駿 on 2025/10/30.
+//
+
+import Foundation
