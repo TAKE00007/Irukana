43259454ee7d37963f48904d420b439d868bf574
TAKE00007 <xshun1590@icloud.com>
2025-11-07 21:50:10 +0900

    カレンダーのフロントの部分を仮で書いた

---
+++ Irukana/Presentation/Calendar/CalendarAction.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarAction.swift
+//  Irukana
+//
+//  Created by 大竹駿 on 2025/11/05.
+//
+
+import Foundation
+
+enum CalendarAction {
+    case onAppear
+}
+
+enum CalendarEffect {
+    case load
+}
---
+++ Irukana/Presentation/Calendar/CalendarState.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarState.swift
+//  Irukana
+//
+//  Created by 大竹駿 on 2025/11/05.
+//
+
+import Foundation
+
+struct CalendarState {
+    var dinnerStatuses: [DinnerStatus]
+}
