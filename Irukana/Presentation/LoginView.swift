f6bbf64921020951347e185321db3c145d2bfce2
TAKE00007 <xshun1590@icloud.com>
2025-11-24 15:17:55 +0900

    アプリアイコンをImageに追加

---
+++ Irukana/Presentation/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Irukana
+//
+//  Created by 大竹駿 on 2025/11/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
