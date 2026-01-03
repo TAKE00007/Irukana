06f9da0bd1d4af2718a1eda5fdd42b5390d0798c
TAKE00007 <xshun1590@icloud.com>
2026-01-03 12:35:06 +0900

    User型の情報をAppRootViewに注入できるようにした

--- Irukana/Presentation/Root/RootView.swift
+++ Irukana/Presentation/Root/RootView.swift
@@ -1,18 +1,8 @@
-//
-//  RootView.swift
-//  Irukana
-//
-//  Created by 大竹駿 on 2025/12/04.
-//
-
 import SwiftUI
 
 struct RootView: View {
     @Environment(\.injected) private var container
     @State private var state: RootState = .loggedOut
-    
-    // userを仮で作る
-//    let user = User(id: UUID(uuidString: "8DFFC7EA-A842-48CF-963B-96714E3E9935")!, name: "Take", passwordHash: "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08", photoURL: nil, birthday: nil)
     
     var body: some View {
         VStack {
@@ -24,7 +14,7 @@ struct RootView: View {
                     state = .loggedIn(user)
                 }
             case .loggedIn(let user):
-                AppRootView() // 将来的にuserを注入するようにする
+                AppRootView(user: user)
             }
         }
         .task {
