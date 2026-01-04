5406deb96fcf26c54abf638e6bfb7aa81ef30faa
TAKE00007 <xshun1590@icloud.com>
2026-01-04 10:02:16 +0900

    dinnerStatusを取得するときに、user情報も取得するようにした

--- Irukana/Application/Service/DinnerStatusService.swift
+++ Irukana/Application/Service/DinnerStatusService.swift
@@ -1,25 +1,45 @@
-//
-//  UpsertDinnerStatutsService.swift
-//  Irukana
-//
-//  Created by 大竹駿 on 2025/10/29.
-//
-
 import Foundation
 
 struct DinnerStatusService {
-    let repository: DinnerStatusRepository
+    let dinnerStatusRepository: DinnerStatusRepository
+    let userRepository: UserRepository
     
     func upsertDinnerStatus(groupId: UUID, date: Date, userId: UUID, isYes: Bool) async throws {
         let answer: DinnerAnswer = isYes ? .need : .noneed
-        try await repository.upsertAnswer(groupId: groupId, date: date, userId: userId, answer: answer)
+        try await dinnerStatusRepository.upsertAnswer(groupId: groupId, date: date, userId: userId, answer: answer)
     }
     
     func loadDinnerStatus(groupId: UUID, date: Date) async throws -> DinnerStatus? {
-        return try await repository.fetch(groupId: groupId, date: date)
+        return try await dinnerStatusRepository.fetch(groupId: groupId, date: date)
+    }
+    
+    func loadDinnerStatusWithUsers(groupId: UUID, date: Date) async throws -> (DinnerStatus, [User])? {
+        guard let dinnerStatus = try await dinnerStatusRepository.fetch(groupId: groupId, date: date)
+        else { return nil }
+        let userIds = Array(dinnerStatus.answers.keys)
+        
+        let users: [User] = try await withThrowingTaskGroup(of: User.self) { group in
+            for userId in userIds {
+                group.addTask {
+                    guard let user = try await userRepository.fetchUser(id: userId)
+                    else { throw UserError.userNotFound }
+                    return user
+                }
+            }
+
+            var fetchedUsers: [User] = []
+            
+            for try await user in group {
+                fetchedUsers.append(user)
+            }
+
+            return fetchedUsers
+        }
+        
+        return (dinnerStatus, users)
     }
     
     func loadDinnerStatusMonth(groupId: UUID, date: Date) async throws -> [DinnerStatus]? {
-        return try await repository.fetchMonth(groupId: groupId, anyDayInMonth: date)
+        return try await dinnerStatusRepository.fetchMonth(groupId: groupId, anyDayInMonth: date)
     }
 }
