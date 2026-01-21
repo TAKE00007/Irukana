b6c7762b685f94041fded242d98ff668ab78bda5
TAKE00007 <xshun1590@icloud.com>
2026-01-21 22:13:46 +0900

    userDefaultsからuserIDを取得する処理を記述

--- Irukana/Application/Service/AuthService.swift
+++ Irukana/Application/Service/AuthService.swift
@@ -1,22 +1,25 @@
-//
-//  UserService.swift
-//  Irukana
-//
-//  Created by 大竹駿 on 2025/11/24.
-//
-
 import Foundation
 
 struct AuthService {
-    let repository: AuthRepository
+    let authRepository: AuthRepository
+    let sessionRepository: SessionRepository
+    let userRepository: UserRepository
     
     func login(name: String, password: String) async throws -> User {
-        let user = try await repository.login(name: name, password: password)
+        let user = try await authRepository.login(name: name, password: password)
         return user
     }
     
     func signUp(name: String, password: String, birthday: Date?) async throws -> User {
-        let user = try await repository.signUp(name: name, password: password, birthday: birthday)
+        let user = try await authRepository.signUp(name: name, password: password, birthday: birthday)
+        sessionRepository.saveUserId(user.id.uuidString)
+        return user
+    }
+    
+    func restoreSession() async throws -> User? {
+        guard let userId = sessionRepository.loadUserId() else { return nil }
+        guard let uuid = UUID(uuidString: userId) else { return nil }
+        let user =  try await userRepository.fetchUser(id: uuid)
         return user
     }
 }
