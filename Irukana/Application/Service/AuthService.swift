df647e3007867af4d153bb3b26fb4da61e2ba287
TAKE00007 <xshun1590@icloud.com>
2026-01-25 11:49:28 +0900

    calendarInfoも取得して自動でログインできるようにした

--- Irukana/Application/Service/AuthService.swift
+++ Irukana/Application/Service/AuthService.swift
@@ -4,6 +4,8 @@ struct AuthService {
     let authRepository: AuthRepository
     let sessionRepository: SessionRepository
     let userRepository: UserRepository
+    let groupRepository: GroupRepository
+    let calendarRepository: CalendarRepository
     
     func login(name: String, password: String) async throws -> User {
         let user = try await authRepository.login(name: name, password: password)
@@ -16,10 +18,17 @@ struct AuthService {
         return user
     }
     
-    func restoreSession() async throws -> User? {
+    func autoLogin() async throws -> (User, [CalendarInfo])? {
         guard let userId = sessionRepository.loadUserId() else { return nil }
         guard let uuid = UUID(uuidString: userId) else { return nil }
-        let user =  try await userRepository.fetchUser(id: uuid)
-        return user
+        guard let user =  try await userRepository.fetchUser(id: uuid) else { return nil }
+        let groupIds =  try await groupRepository.fetchGroupIds(userId: uuid) // 今は1個しか返ってこない
+        var calendarInfos: [CalendarInfo] = []
+        for groupId in groupIds {
+            guard let calendrInfo = try await calendarRepository.fetchCalendarByGroupId(groupId: groupId) else { continue }
+            calendarInfos.append(calendrInfo)
+        }
+
+        return (user, calendarInfos)
     }
 }
