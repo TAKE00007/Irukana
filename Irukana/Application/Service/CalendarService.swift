dddcf9d2280ae0e341d268e41f04a083632494cf
TAKE00007 <xshun1590@icloud.com>
2026-01-27 21:27:37 +0900

    CalendarServiceでカレンダーに参加しているUserを取得できるようにした

--- Irukana/Application/Service/CalendarService.swift
+++ Irukana/Application/Service/CalendarService.swift
@@ -3,6 +3,7 @@ import Foundation
 struct CalendarService {
     let calendarRepository: CalendarRepository
     let groupRepository: GroupRepository
+    let userRepository: UserRepository
     
     func createCalendarInfo(userId: UUID, name: String) async throws -> CalendarInfo {
         let id = UUID()
@@ -23,5 +24,29 @@ struct CalendarService {
         try await groupRepository.addGroup(userId: userId, groupId: calendarInfo.groupId)
         
         return calendarInfo
+    }
+
+    func loadUsers(groupId: UUID) async throws -> [User] {
+        let userIds = try await groupRepository.fetchUserIds(groupId: groupId)
+        guard !userIds.isEmpty else { return [] }
+        
+        let users: [User] = try await withThrowingTaskGroup(of: User.self) { group in
+            for userId in userIds {
+                group.addTask {
+                    guard let user = try await userRepository.fetchUser(id: userId)
+                    else { throw UserError.userNotFound }
+                    return user
+                }
+            }
+            
+            var fetcedUsers: [User] = []
+            
+            for try await user in group {
+                fetcedUsers.append(user)
+            }
+            
+            return fetcedUsers
+        }
+        return users
     }
 }
