9a600f0650d1f3f349bbaccf5b19eac10eb43d86
TAKE00007 <xshun1590@icloud.com>
2026-02-03 22:37:06 +0900

    新しくサービス層を作った

--- Irukana/Application/Service/LocalNotificationService.swift
+++ Irukana/Application/Service/LocalNotificationService.swift
@@ -1,8 +1,22 @@
-//
-//  LocalNotificationService.swift
-//  Irukana
-//
-//  Created by 大竹駿 on 2026/02/03.
-//
-
 import Foundation
+
+struct LocalNotificationService {
+    let localNotificationRepository: LocalNotificationRepository
+    let sessionRepository: SessionRepository
+    
+    func updateIsNotification(isNotification: Bool) {
+        if !isNotification {
+            localNotificationRepository.removeDinnerNotification()
+        }
+
+        sessionRepository.saveIsNotification(isNotification)
+    }
+    
+    func loadIsNotification() -> Bool? {
+        return sessionRepository.loadIsNotification()
+    }
+    
+    func setDinnerNotification(notificationAt: Date) async {
+        await localNotificationRepository.setDinnerNotification(notificationAt: notificationAt)
+    }
+}
