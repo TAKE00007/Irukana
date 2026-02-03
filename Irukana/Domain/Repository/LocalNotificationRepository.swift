add86df8951cf85a70b9258a76ba0eee429f1bc4
TAKE00007 <xshun1590@icloud.com>
2026-02-03 22:36:32 +0900

    任意の時間で予約できるようにした

--- Irukana/Domain/Repository/LocalNotificationRepository.swift
+++ Irukana/Domain/Repository/LocalNotificationRepository.swift
@@ -2,4 +2,6 @@ import Foundation
 
 protocol LocalNotificationRepository {
     func initSetup() async
+    func setDinnerNotification(notificationAt: Date) async
+    func removeDinnerNotification()
 }
--- Irukana/Domain/RepositoryImp/LocalNotificationRepositoryImp.swift
+++ Irukana/Domain/RepositoryImp/LocalNotificationRepositoryImp.swift
@@ -47,6 +47,57 @@ struct LocalNotificationRepositoryImp: LocalNotificationRepository {
         }
     }
     
+    // TODO: 処理が重複しているので後で直す
+    func setDinnerNotification(notificationAt: Date) async {
+        let center = UNUserNotificationCenter.current()
+        let status = await checkAuthorizationStatus(center: center)
+        
+        switch status {
+        case .notDetermined:
+            do {
+                let granted = try await center.requestAuthorization(options: [.alert, .sound, .badge])
+                guard granted else { return }
+            } catch {
+                return
+            }
+        default:
+            return
+        }
+        
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour, .minute], from: notificationAt)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "確認"
+        content.body = "今日のご飯、どうしますか？"
+        content.sound = .default
+        
+        content.userInfo = [
+            "route": "dinner"
+        ]
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
+        
+        let request = UNNotificationRequest(
+            identifier: "dinner.reminder.daily",
+            content: content,
+            trigger: trigger
+        )
+        
+        do {
+            try await center.add(request)
+        } catch {
+            print("通知データを作れなかった")
+        }
+    }
+    
+    func removeDinnerNotification() {
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(
+            withIdentifiers: ["dinner.reminder.daily"]
+        )
+    }
+    
     private func checkAuthorizationStatus(center: UNUserNotificationCenter) async -> UNAuthorizationStatus {
         let setting = await center.notificationSettings()
         
